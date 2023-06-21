595c11a04f000d5b47a3ffd761e4a085f3ffc59a
RMrupoma <124458630+RMrupoma@users.noreply.github.com>
2023-06-21 16:22:57 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//jq
+$(document).ready(function()
+{
+  
+    var arc = function (t)
+    {
+        return {
+            x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+            y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+        };
+   
+
+    $('#text1').curvedText({
+        curve:    arc,
+        domain:   [-0.2, 0.2],
+       viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
+
+
+ };
+   wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
